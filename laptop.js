0d71301a4a8269345823dad3c4e2155560fb9e9f
Administrator <Administrator@DESKTOP-IVHRL9V>
2025-04-14 12:20:50 +0300

    Monday

--- laptop.js
+++ laptop.js
@@ -3,7 +3,7 @@ let products = [
         title: " 2015 Apple Macbook Air",
         description: "A powerful laptop for all your needs.",
         price: "KSh:53,000",
-        
+        category: "Macbook", // Added Category
         images: [
             "Stock/mACBOOK AIR 2015/PIC1.jpg",
             "Stock/mACBOOK AIR 2015/PIC2.jpg",
@@ -19,11 +19,11 @@ let products = [
             screen: "13.3 inch FHD"
         }
     },
-        {
+    {
         title: "Lenovo Thinkpad Yoga 11e",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:16,500", // Consider adding currency
-        
+        price: "KSh:16,500",
+        category: "Thinkpad", // Added Category
         images: [
             "Stock/lenovo thinkpad yoga 11e/pic5.jpg",
             "Stock/lenovo thinkpad yoga 11e/pic4.jpg",
@@ -43,8 +43,8 @@ let products = [
     {
         title: "HP EliteBook 1040 G3",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:39,000", // Consider adding currency
-        
+        price: "KSh:39,000",
+        category: "HP Laptops", // Added Category
         images: [
             "Stock/hp1040 g3/pic4.webp",
             "Stock/hp1040 g3/pic4.jpg",
@@ -59,11 +59,11 @@ let products = [
             screen: "35.6 cm (14) LED backlight 16:9",
         }
     },
-        {
+    {
         title: "HP PROBOOK 635 G7",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:39,500", // Consider adding currency
-        
+        price: "KSh:39,500",
+        category: "HP Laptops", // Added Category
         images: [
             "Stock/HP PROBOOK 635 G7 Ryzen 16256 ssd/pic5.jpg",
             "Stock/HP PROBOOK 635 G7 Ryzen 16256 ssd/pic4.jpg",
@@ -82,10 +82,10 @@ let products = [
     {
         title: "HP PROBOOK 445 G9 Ryzen 7",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:55,000", // Consider adding currency
-       
+        price: "KSh:55,000",
+        category: "HP Laptops",  // Added Category
         images: [
-           "Stock/HP PROBOOK 445 G9 Ryzen 7 16256 ssd touch screen/pic5.jpg",
+            "Stock/HP PROBOOK 445 G9 Ryzen 7 16256 ssd touch screen/pic5.jpg",
             "Stock/HP PROBOOK 445 G9 Ryzen 7 16256 ssd touch screen/pic4.jpg",
             "Stock/HP PROBOOK 445 G9 Ryzen 7 16256 ssd touch screen/pic3.jpg",
             "Stock/HP PROBOOK 445 G9 Ryzen 7 16256 ssd touch screen/pic2.jpg",
@@ -95,25 +95,25 @@ let products = [
             cpu: "Intel Core i7", // This doesn't match the title "Ryzen 7" - please verify
             ram: "16GB DDR4",
             storage: "256GB SSD",
-            graphics:"AMD Radeon Graphics",
+            graphics: "AMD Radeon Graphics",
             screen: "14inch FHD"
         }
     },
     {
         title: "HP PROBOOK 440 G5",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:42,000", // Consider adding currency
-        
+        price: "KSh:42,000",
+        category: "HP Laptops", // Added Category
         images: [
             "Stock/HP PROBOOK 440 G5/pic1.jpg",
             "Stock/HP PROBOOK 440 G5/pic2.webp",
             "Stock/HP PROBOOK 440 G5/pic3.jpg",
-            "Stock/HP PROBOOK 440 G5/pic2.jpg", // Duplicate image path
+            "Stock/HP PROBOOK 440 G5/pic2.jpg",
             "Stock/HP PROBOOK 440 G5/pic4.jpg",
             "Stock/HP PROBOOK 440 G5/pic5.jpg",
         ],
         specs: {
-            cpu: "Intel Core i5-8500h, 8th generation", // Typo? Should it be i5-8250U as in other similar models?
+            cpu: "Intel Core i5-8500h, 8th generation", // Typo? Should it be i5-8250U?
             ram: "8GB DDR4",
             storage: "256GB SSD",
             graphics: "Intel Integrated UHD 620",
@@ -123,8 +123,8 @@ let products = [
     {
         title: "HP ELITEBOOK 840 G5",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:35,000", // Consider adding currency
-        
+        price: "KSh:35,000",
+        category: "HP Laptops", // Added Category
         images: [
             "Stock/HP ELITEBOOK 840 G5/pic4.jpg",
             "Stock/HP ELITEBOOK 840 G5/pic5.jpg",
@@ -136,15 +136,15 @@ let products = [
             cpu: "Intel Core i5-8650U ",
             ram: "8GB DDR4",
             storage: "256GB SSD",
-            graphics: "Intel UHD 620.", // Note the trailing dot
+            graphics: "Intel UHD 620.",
             screen: "14-inch FHD"
         }
     },
     {
         title: "HP ELITEBOOK 840 G2",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:28,500", // Consider adding currency
-        
+        price: "KSh:28,500",
+        category: "HP Laptops", // Added Category
         images: [
             "Stock/HP ELITEBOOK 840 G2, 5TH GEN/pic5.jpg",
             "Stock/HP ELITEBOOK 840 G2, 5TH GEN/pic4.jpg",
@@ -157,16 +157,16 @@ let products = [
             ram: "8GB DDR4",
             storage: "128GB SSD",
             graphics: "Intel HD 5500",
-            screen: " 14inch FHD " // Note the extra space at the end
+            screen: " 14inch FHD "
         }
     },
     {
-        title: "HP ELITEBOOK 830 G7 X360 ", // Duplicate title
+        title: "HP ELITEBOOK 830 G7 X360 ",
         description: "A powerful laptop for all your needs.",
-        price: "KSh: 57,000", // Consider adding currency
-        
+        price: "KSh: 57,000",
+        category: "HP Laptops", // Added Category
         images: [
-            " Stock/HP ELITEBOOK 830 G7 X360 core i 5 10th gen 16512/pic5.jpg", // Note the leading space in the path
+            " Stock/HP ELITEBOOK 830 G7 X360 core i 5 10th gen 16512/pic5.jpg",
             "Stock/HP ELITEBOOK 830 G7 X360 core i 5 10th gen 16512/pic4.png",
             "Stock/HP ELITEBOOK 830 G7 X360 core i 5 10th gen 16512/pic3.jpg",
             "Stock/HP ELITEBOOK 830 G7 X360 core i 5 10th gen 16512/pic2.jpg",
@@ -181,10 +181,10 @@ let products = [
         }
     },
     {
-        title: "HP X360 1030 G3", // Duplicate title
+        title: "HP X360 1030 G3",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:45,000", // Consider adding currency
-        
+        price: "KSh:45,000",
+        category: "HP Laptops", // Added Category
         images: [
             "Stock/HP 830 G5/pic1.jpg",
             "Stock/HP 830 G5/pic2.jpg",
@@ -196,21 +196,21 @@ let products = [
             cpu: "Intel Core i7",
             ram: "16GB DDR4",
             storage: "512GB SSD",
-            graphics: "NVIDIA GeForce RTX 3060", // High-end GPU for a "general" powerful laptop? Verify target audience.
+            graphics: "NVIDIA GeForce RTX 3060",
             screen: "15.6\" FHD"
         }
     },
     {
         title: "HP Elitebook 745 G5",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:34,500", // Consider adding currency
-        
+        price: "KSh:34,500",
+        category: "HP Laptops", // Added Category
         images: [
-            "Stock/HP 745 G5,/pic1.jpg", // Note the comma in the path
-            "Stock/HP 745 G5,/pic2.jpg", // Note the comma in the path
-            "Stock/HP 745 G5,/pic3.jpg", // Note the comma in the path
-            "Stock/HP 745 G5,/pic4.jpg", // Note the comma in the path
-            "Stock/HP 745 G5,/pic5.jpg", // Note the comma in the path
+            "Stock/HP 745 G5,/pic1.jpg",
+            "Stock/HP 745 G5,/pic2.jpg",
+            "Stock/HP 745 G5,/pic3.jpg",
+            "Stock/HP 745 G5,/pic4.jpg",
+            "Stock/HP 745 G5,/pic5.jpg",
         ],
         specs: {
             cpu: "AMD Ryzen™ 7 PRO",
@@ -223,28 +223,28 @@ let products = [
     {
         title: "HP Probook 430 G5",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:28,000", // Consider adding currency
-        
+        price: "KSh:28,000",
+        category: "HP Laptops", // Added Category
         images: [
-            "Stock/HP 430, G5/pic1.jpg", // Note the comma in the path
-            "Stock/HP 430, G5/pic2.jpg", // Note the comma in the path
-            "Stock/HP 430, G5/pic3.jpg", // Note the comma in the path
-            "Stock/HP 430, G5/pic4.png", // Note the comma in the path
-            "Stock/HP 430, G5/pic5.jpg", // Note the comma in the path
+            "Stock/HP 430, G5/pic1.jpg",
+            "Stock/HP 430, G5/pic2.jpg",
+            "Stock/HP 430, G5/pic3.jpg",
+            "Stock/HP 430, G5/pic4.png",
+            "Stock/HP 430, G5/pic5.jpg",
         ],
         specs: {
-            cpu: " Intel Core i5-8250U @1.6GHz", // Note the leading space
+            cpu: " Intel Core i5-8250U @1.6GHz",
             ram: "8GB DDR4",
             storage: "128GB SSD",
             graphics: "Intel UHD Graphics 620",
-            screen: "13.3inch Full HD " // Note the trailing space
+            screen: "13.3inch Full HD "
         }
     },
     {
         title: "DELL 7390",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:36,000", // Consider adding currency
-        
+        price: "KSh:36,000",
+        category: "Dell Laptops", // Added Category
         images: [
             "Stock/DELL 7390/pic1.jpg",
             "Stock/DELL 7390/pic2.jpg",
@@ -257,18 +257,18 @@ let products = [
             ram: "8GB DDR4",
             storage: "256GB SSD",
             graphics: "Intel® UHD Graphics 620",
-            screen: "13.3inch(33.8cm) " // Note the trailing space
+            screen: "13.3inch(33.8cm) "
         }
     },
     {
         title: "DELL 5490",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:40,500", // Consider adding currency
-        
+        price: "KSh:40,500",
+        category: "Dell Laptops", // Added Category
         images: [
             "Stock/DELL 5490/pic1.jpg",
             "Stock/DELL 5490/pic2.jpg",
-            "Stock/DELL 5490/pic2.png", // Mixed image types
+            "Stock/DELL 5490/pic2.png",
             "Stock/DELL 5490/pic3.jpg",
             "Stock/DELL 5490/pic4.jpg",
             "Stock/DELL 5490/pic5.jpg",
@@ -279,14 +279,14 @@ let products = [
             ram: "16GB DDR4",
             storage: "256GB SSD",
             graphics: "Intel® UHD Graphics 620",
-            screen: " 14 inches (35.56 cm)" // Note the leading space
+            screen: " 14 inches (35.56 cm)"
         }
     },
     {
         title: "DELL 3300",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:21,600", // Consider adding currency
-        
+        price: "KSh:21,600",
+        category: "Dell Laptops", // Added Category
         images: [
             "Stock/DELL 3300/pic1.jpg",
             "Stock/DELL 3300/pic2.jpg",
@@ -298,19 +298,19 @@ let products = [
             cpu: "Intel® Core™ i3 processor",
             ram: "4GB DDR4",
             storage: "128GB SSD",
-            graphics: " Integrated: Intel® HD 620 Graphics", // Note the leading space
-            screen: "13.3 inches (33.78 cm) " // Note the trailing space
+            graphics: " Integrated: Intel® HD 620 Graphics",
+            screen: "13.3 inches (33.78 cm) "
         }
     },
     {
         title: "Acer Chromebook",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:17,000", // Consider adding currency
-        
+        price: "KSh:17,000",
+        category: "Chromebook", // Added Category
         images: [
             "Stock/Acer Chromebook/pic4.webp",
             "Stock/Acer Chromebook/pic2.webp",
-            "Stock/Acer Chromebook/pic 1.webp", // Note the space in the filename
+            "Stock/Acer Chromebook/pic 1.webp",
             "Stock/Acer Chromebook/pic3.webp"
         ],
         specs: {
@@ -321,41 +321,41 @@ let products = [
             screen: "11.6 inch Touchscreen"
         }
     },
-      {
+    {
         title: "DELL 7389",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:36,000", // Consider adding currency
-        
+        price: "KSh:36,000",
+        category: "Dell Laptops",  // Added Category
         images: [
-            "Stock/Dell 7389/pic 1.png", // Note the space in the filename
+            "Stock/Dell 7389/pic 1.png",
             "Stock/Dell 7389/pic2.png",
             "Stock/Dell 7389/pic3.png",
             "Stock/Dell 7389/pic4.jpg",
             "Stock/Dell 7389/pic5.jpg",
         ],
         specs: {
-            cpu: "Intel Core \u00A0i5 7th gen", // Note the non-breaking space
+            cpu: "Intel Core \u00A0i5 7th gen",
             ram: "8GB DDR4",
             storage: "256GB SSD",
             graphics: "Intel® UHD Graphics 620",
             screen: "13.3” FHD (1920x1080) Touch X30"
         }
     },
-    
+
     {
         title: "HP Zbook 14u G5",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:36,000", // Consider adding currency
-        
+        price: "KSh:36,000",
+        category: "HP Laptops", // Added Category
         images: [
-            "Stock/HP z book 14u G5/pic1.jpg", // Note the lowercase 'z'
-            "Stock/HP z book 14u G5/pic2.jpg", // Note the lowercase 'z'
-            "Stock/HP z book 14u G5/pic3.jpg", // Note the lowercase 'z'
-            "Stock/HP z book 14u G5/pic4.jpg", // Note the lowercase 'z'
-            "Stock/HP z book 14u G5/pic5.jpg", // Note the lowercase 'z'
+            "Stock/HP z book 14u G5/pic1.jpg",
+            "Stock/HP z book 14u G5/pic2.jpg",
+            "Stock/HP z book 14u G5/pic3.jpg",
+            "Stock/HP z book 14u G5/pic4.jpg",
+            "Stock/HP z book 14u G5/pic5.jpg",
         ],
         specs: {
-            cpu: "Intel Core \u00A0i5 8th gen", // Note the non-breaking space
+            cpu: "Intel Core \u00A0i5 8th gen",
             ram: "8GB DDR4",
             storage: "256GB SSD",
             graphics: "Intel® UHD Graphics 620",
@@ -365,13 +365,13 @@ let products = [
     {
         title: "HP EliteBook 840 G4",
         description: "A powerful laptop for all your needs.",
-        price: "KSh:36,000", // Consider adding currency
-       
+        price: "KSh:36,000",
+        category: "HP Laptops", // Added Category
         images: [
-            "Stock/HP EliteBook 840, G4/pic4.jpg", // Note the comma
-            "Stock/HP EliteBook 840, G4/pic3.jpg", // Note the comma
-            "Stock/HP EliteBook 840, G4/pic2.jpg", // Note the comma
-            "Stock/HP EliteBook 840, G4/pic1.jpg", // Note the comma
+            "Stock/HP EliteBook 840, G4/pic4.jpg",
+            "Stock/HP EliteBook 840, G4/pic3.jpg",
+            "Stock/HP EliteBook 840, G4/pic2.jpg",
+            "Stock/HP EliteBook 840, G4/pic1.jpg",
         ],
         specs: {
             cpu: "Intel Core i5-7300U",
@@ -381,24 +381,42 @@ let products = [
             screen: "35.6 cm (14) Full HD"
         }
     },
+    {
+        title: "HP Elitebook 745 G5",
+        description: "A powerful laptop for all your needs.",
+        price: "KSh:40,000",
+        category: "HP Laptops", // Added Category
+        images: [
+            "Stock/HP 745 G5,/pic1.jpg",
+            "Stock/HP 745 G5,/pic2.jpg",
+            "Stock/HP 745 G5,/pic3.jpg",
+            "Stock/HP 745 G5,/pic4.jpg",
+            "Stock/HP 745 G5,/pic5.jpg",
+        ],
+        specs: {
+            cpu: "AMD Ryzen™ 7 PRO",
+            ram: "16GB DDR4",
+            storage: "512GB SSD",
+            graphics: "AMD Radeon™ Vega 10 Graphics",
+            screen: "14 inch FHD"
+        }
+    }
 
-];
+]
 
 
-// Function to create product cards
 const productGrid = document.getElementById('productGrid');
-if (!productGrid) {
-    console.error("Product grid element not found!");
-}
 
-function createProductCards() {
-    if (!productGrid) return; // Exit if productGrid is null
-
+function createProductCards(productsToDisplay = products) {
+    if (!productGrid) {
+        console.error("Product grid element not found!");
+        return;
+    }
     productGrid.innerHTML = '';
 
-    const itemListElement = []; // For schema
+    const itemListElement = [];
 
-    products.forEach((product, index) => { // Added index
+    productsToDisplay.forEach((product, index) => {
         let imagesHtml = product.images.map(img => `<img src="${img}" alt="${product.title}">`).join('');
 
         let specsHtml = Object.entries(product.specs).map(([key, value]) => {
@@ -437,17 +455,15 @@ function createProductCards() {
 
         productGrid.appendChild(card);
 
-        // 9. Add Product Schema Dynamically
         itemListElement.push({
             "@type": "ListItem",
             "position": index + 1,
             "name": product.title,
             "description": product.description,
-            "url": `product/${product.id}.html` // Create specific product pages!
+            "url": `product/${product.id}.html`
         });
     });
 
-    // Update Schema
     const schemaElement = document.querySelector('script[type="application/ld+json"]');
     if (schemaElement) {
         const schemaData = JSON.parse(schemaElement.textContent);
@@ -456,7 +472,7 @@ function createProductCards() {
     }
 }
 
-// Improved slider functions (as you provided)
+
 function prevSlide(button) {
     const slider = button.closest('.product-slider');
     const imagesContainer = slider.querySelector('.product-slider-images');
@@ -491,12 +507,47 @@ function nextSlide(button) {
     }, 10);
 }
 
-// Filter function (example)
-function filterProducts(category) {
-    // In a real implementation, you would filter the products array
-    console.log(`Filtering by: ${category}`);
-    createProductCards(); // Re-render with filtered products
-}
 
-// Initialize the page
-document.addEventListener('DOMContentLoaded', createProductCards);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const categoryList = document.getElementById('category-list');
+    if (!categoryList) {
+        console.error("Category list element not found!");
+        return;
+    }
+
+    function getCategories(products) {
+        let categories = new Set();
+        products.forEach(product => {
+            categories.add(product.category);
+        });
+        return Array.from(categories);
+    }
+
+    const categories = getCategories(products);
+
+    function generateCategoryList(categories) {
+        let html = categories.map(category =>
+            `<li data-category="${category}">${category}</li>`
+        ).join('');
+        categoryList.innerHTML = html;
+    }
+
+    generateCategoryList(categories);
+
+    categoryList.addEventListener('click', (event) => {
+        if (event.target.tagName === 'LI') {
+            const selectedCategory = event.target.dataset.category;
+            filterProducts(selectedCategory);
+        }
+    });
+
+    function filterProducts(category) {
+        const filteredProducts = products.filter(product => {
+            return product.category.toLowerCase() === category.toLowerCase();
+        });
+        createProductCards(filteredProducts);
+    }
+
+    createProductCards();
+});
